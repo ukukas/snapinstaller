7d07163be2e2d254c1ce209addd395ac90044800
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2021-06-21 15:18:04 -0400

    add configuration files

--- install.ps1
+++ install.ps1
@@ -1,6 +1,22 @@
 $installer = Resolve-Path ".\esa-snap_all_windows-x64_8_0.exe"
 $varfile = Resolve-Path ".\response.varfile"
 
+$snap_config_file = Resolve-Path ".\config\snap.properties"
+$rcp_config_file = Resolve-Path ".\config\rcp.properties"
+$update_config_file = Resolve-Path ".\config\autoupdate.properties"
+
+$snap_config_dir = "C:\Users\Default\.snap\etc"
+$rcp_config_dir = "C:\Users\Default\AppData\Roaming\SNAP\config\Preferences\org\esa\snap\snap"
+$update_config_dir = "C:\Users\Default\AppData\Roaming\SNAP\config\Preferences\org\netbeans\modules"
+
 $arglist = "-q -varfile $varfile"
 
 Start-Process -FilePath $installer -ArgumentList $arglist -NoNewWindow -Wait
+
+New-Item -Path $snap_config_dir -ItemType "directory" -Force
+New-Item -Path $rcp_config_dir -ItemType "directory" -Force
+New-Item -Path $update_config_dir -ItemType "directory" -Force
+
+Copy-Item -Path $snap_config_file -Destination $snap_config_dir -Force
+Copy-Item -Path $rcp_config_file -Destination $rcp_config_dir -Force
+Copy-Item -Path $update_config_file -Destination $update_config_dir -Force
