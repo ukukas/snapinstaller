28ada4a720e4f9c3a04249a44e0972bd317932f4
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2021-06-21 16:17:21 -0400

    add section to update modules using local mirror

--- install.ps1
+++ install.ps1
@@ -2,6 +2,9 @@ $installer = Resolve-Path ".\esa-snap_all_windows-x64_8_0.exe"
 $varfile = Resolve-Path ".\response.varfile"
 $mirror = Resolve-Path ".\step.esa.int"
 
+$snapexe = "C:\Program Files\snap\bin\snap64.exe"
+$process = "snap64"
+
 $snap_config_file = Resolve-Path ".\config\snap.properties"
 $rcp_config_file = Resolve-Path ".\config\rcp.properties"
 $autoupdate_config_file = Resolve-Path ".\config\autoupdate.properties"
@@ -10,9 +13,7 @@ $snap_config_dir = "C:\Users\Default\.snap\etc"
 $rcp_config_dir = "C:\Users\Default\AppData\Roaming\SNAP\config\Preferences\org\esa\snap\snap"
 $autoupdate_config_dir = "C:\Users\Default\AppData\Roaming\SNAP\config\Preferences\org\netbeans\modules"
 
-$arglist = "-q -varfile $varfile"
-
-Start-Process -FilePath $installer -ArgumentList $arglist -NoNewWindow -Wait
+Start-Process -FilePath $installer -ArgumentList "-q -varfile $varfile" -NoNewWindow -Wait
 
 New-Item -Path $snap_config_dir -ItemType "directory" -Force
 New-Item -Path $rcp_config_dir -ItemType "directory" -Force
@@ -43,3 +44,11 @@ New-Item -Path (Join-Path $update_config_dir $core_update_filename) -ItemType "f
 New-Item -Path (Join-Path $update_config_dir $toolboxes_update_filename) -ItemType "file" -Value $toolboxes_update_content -Force
 New-Item -Path (Join-Path $update_config_dir $supported_update_filename) -ItemType "file" -Value $supported_update_content -Force
 New-Item -Path (Join-Path $update_config_dir $community_update_filename) -ItemType "file" -Value $community_update_content -Force
+
+& "$snapexe --console suppress --nosplash --nogui --modules --update-all" | ForEach-Object {
+    if ($_ -eq "updates=0") {
+        Get-Process -Name $process -ErrorAction SilentlyContinue | Stop-Process -Force
+    }
+}
+
+Remove-Item -Path (Join-Path $env:APPDATA "SNAP") -Recurse -Force
